d4622bca9ab9178845950cf3e48750ed6c2310de
System Administrator <root@MBP-van-Rens.home>
2023-02-10 16:46:53 +0100

    fix nav to home bug

--- components/ThePageHeader.vue
+++ components/ThePageHeader.vue
@@ -11,17 +11,6 @@ export default {
       required: false,
     },
   },
-  data() {
-    return {
-      routeName: this.$route.name,
-      isMobileMenuVisible: false,
-    };
-  },
-  watch: {
-    $route() {
-      this.routeName = this.$route.name;
-    },
-  },
 };
 
 </script>
@@ -29,7 +18,7 @@ export default {
 <template>
   <div class="bg-white">
     <section>
-      <Container class="-mb-5 md:-mb-8 lg:-mb-12 xl:-mb-14" :class="{ '-mb-10': routeName === 'index' }">
+      <Container class="-mb-5 md:-mb-8 lg:-mb-12 xl:-mb-14" :class="{ '-mb-10': showImage }">
         <div class="flex flex-col justify-between md:flex-row md:items-end">
           <h1 class="text-5xl font-bold md:text-7xl lg:text-8xl 2xl:text-9xl shrink" v-html="title">
           </h1>
