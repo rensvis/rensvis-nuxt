858018fa2786b76ec21d130f3ad39cc6d9cb01d0
System Administrator <root@MBP-van-Rens.home>
2023-02-11 12:13:07 +0100

    add max width to page intro

--- components/global/PageIntro.vue
+++ components/global/PageIntro.vue
@@ -2,11 +2,11 @@
 </script>
 
 <template>
-  <div class="px-10">
-    <div class="container sm:ml-auto sm:max-w-md sm:text-right font-bold">
+  <Container>
+    <div class="ml-auto font-bold  sm:max-w-md sm:text-right">
       <div class="content-block">
         <ContentSlot :use="$slots.default" />
       </div>
     </div>
-  </div>
+  </Container>
 </template>
